9710b08583f3e17c2d421799a769c93b191a9a4c
Andrés Court <andres@andrescourt.com>
2023-02-11 23:11:56 -0500

    Loading data directly from the client

--- app/helpers/getCompanies.tsx
+++ app/helpers/getCompanies.tsx
@@ -1,9 +1,13 @@
 import { CompanyInterface } from '@/types';
+import prisma from '@/prisma/client';
 
 export async function getCompanies(): Promise<CompanyInterface[]> {
-	const res = await fetch(`${process.env.SERVER}/api/companies/get`);
+	// const res = await fetch(`${process.env.SERVER}/api/companies/get`);
 
-	if (!res.ok) console.log(res);
+	// if (!res.ok) console.error(res);
 
-	return res.json();
+	// return res.json();
+
+	const res = await prisma.company.findMany()
+	return res
 }
